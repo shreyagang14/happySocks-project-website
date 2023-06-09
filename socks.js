af68d65fff281b928cb6b69eb920785302fb2891
shreyagang14 <114137018+shreyagang14@users.noreply.github.com>
2023-06-09 11:33:48 +0530

    Add files via upload

---
+++ socks.js
@@ -0,0 +1,185 @@
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('body'),
+//     smooth: true
+// });
+
+
+
+gsap.to("#page1", {
+    scrollTrigger: {
+        trigger: "#page1",
+        scroller: "body",
+        start: "top 150%",
+       
+
+    },
+
+    opacity: 1,
+    ease: Expo.easeInOut,
+    duration: 1,
+    stagger: 1,
+    delay: 1
+})
+
+gsap.to("#page2", {
+    scrollTrigger: {
+        trigger: "#page2",
+        scroller: "body",
+        start: "top 100%",
+       
+
+
+    },
+
+    opacity: 1,
+    y: 20,
+    ease: "expo.inOut",
+    duration: 2,
+    stagger: 0.07,
+    delay: 1 ,
+    ease: Expo.easeInOut
+
+
+
+
+
+})
+gsap.to("#page3", {
+    scrollTrigger: {
+        trigger: "#page3",
+        scroller: "body",
+        start: "top 100%",
+       
+
+
+    },
+
+    opacity: 1,
+    y: 20,
+    duration: 2,
+    stagger: 0.07,
+    delay: 1 ,
+
+    ease: Expo.easeInOut
+
+
+
+
+
+})
+gsap.to("#page4", {
+    scrollTrigger: {
+        trigger: "#page4",
+        scroller: "body",
+        start: "top 100%",
+       
+
+
+    },
+
+    opacity: 1,
+    y: 20,
+    duration: 2,
+    stagger: 0.07,
+    delay: 1 ,
+
+    ease: Expo.easeInOut
+
+
+
+
+
+})
+gsap.to("#page5", {
+    scrollTrigger: {
+        trigger: "#page5",
+        scroller: "body",
+        start: "top 100%",
+       
+
+
+    },
+
+    opacity: 1,
+    y: 20,
+    duration: 2,
+    stagger: 0.07,
+    delay: 1 ,
+
+    ease: Expo.easeInOut
+
+
+
+
+
+})
+gsap.to("#page6", {
+    scrollTrigger: {
+        trigger: "#page6",
+        scroller: "body",
+        start: "top 100%",
+       
+
+
+    },
+
+    opacity: 1,
+    y: 20,
+    duration: 2,
+    stagger: 0.07,
+    delay: 1 ,
+
+    ease: Expo.easeInOut
+
+
+
+
+
+})
+gsap.to("#page7", {
+    scrollTrigger: {
+        trigger: "#page7",
+        scroller: "body",
+        start: "top 100%",
+       
+
+
+    },
+
+    opacity: 1,
+    y: 20,
+    duration: 2,
+    stagger: 0.07,
+    delay: 1 ,
+
+    ease: Expo.easeInOut
+
+
+
+
+
+})
+gsap.to("#page8", {
+    scrollTrigger: {
+        trigger: "#page8",
+        scroller: "body",
+        start: "top 100%",
+     
+
+
+    },
+
+    opacity: 1,
+    y: 20,
+    duration: 2,
+    stagger: 0.07,
+    delay: 1 ,
+
+    ease: Expo.easeInOut
+
+
+
+
+
+})
+
